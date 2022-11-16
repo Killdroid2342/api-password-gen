254942e64943c03d48358b18125da39258d707c3
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-16 01:39:35 +0000

    deleted styles

--- src/PasswordGen/PasswordGenAPI.jsx
+++ src/PasswordGen/PasswordGenAPI.jsx
@@ -11,17 +11,13 @@ const PasswordGen = () => {
   };
   return (
     <div className='flex flex-col items-center justify-center my-auto h-screen'>
-      <div className=' border-2 border-white w-1/2 rounded-md flex flex-col items-center pb-24'>
-        <h1 className='font-bold text-3xl text-white mt-10'>
-          Password Generator
-        </h1>
-        <p className='border-2 border-white text-white p-3 rounded-2xl mt-10 font-bold '>
-          {password}
-        </p>
-        <button onClick={getRandomPassword} className='btn'>
+      <div className=' border-2 border-white'>
+        <h1 className=''>Password Generator</h1>
+        <p className='border-2 border-white'>{password}</p>
+        <button onClick={getRandomPassword} className=''>
           Generate Password
         </button>
-        <button className='btn' onClick={savePassword}>
+        <button className='' onClick={savePassword}>
           Copy Password
         </button>
       </div>
