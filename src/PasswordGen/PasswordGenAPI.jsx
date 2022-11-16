6b2d0781dd46e81c792f4c13565e8b6fb05542fe
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-16 00:28:13 +0000

    done jsx and styling

---
+++ src/PasswordGen/PasswordGenAPI.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const PasswordGen = () => {
+  document.title = 'Password Generator';
+  const [password, setPassword] = useState('Click Button To Generate Password');
+
+  const getRandomPassword = () => {};
+  const savePassword = () => {
+    navigator.clipboard.writeText(password);
+    alert(`This is the password that you have copied: ${password}`);
+  };
+  return (
+    <div className='flex flex-col items-center justify-center my-auto h-screen'>
+      <div className=' border-2 border-white w-1/2 rounded-md flex flex-col items-center pb-24'>
+        <h1 className='font-bold text-3xl text-white mt-10'>
+          Password Generator
+        </h1>
+        <p className='border-2 border-white text-white p-3 rounded-2xl mt-10 font-bold '>
+          {password}
+        </p>
+        <button onClick={getRandomPassword} className='btn'>
+          Generate Password
+        </button>
+        <button className='btn' onClick={savePassword}>
+          Copy Password
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PasswordGen;
