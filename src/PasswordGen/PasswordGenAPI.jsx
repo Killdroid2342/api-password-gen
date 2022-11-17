68676fd314e5db25a1e9a267e41304c4af919a0c
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-17 22:46:43 +0000

    added more styling

--- src/PasswordGen/PasswordGenAPI.jsx
+++ src/PasswordGen/PasswordGenAPI.jsx
@@ -10,18 +10,18 @@ const PasswordGen = () => {
     alert(`This is the password that you have copied: ${password}`);
   };
   return (
-    <div className='flex flex-col items-center justify-center my-auto h-screen'>
-      <div className=' border-2 border-white flex flex-col'>
-        <h1 className=''>Password Generator</h1>
-        <p className='border-2 border-white'>{password}</p>
+    <div className='flex flex-col items-center justify-center my-auto h-screen text-center '>
+      <div className=' border-2 border-white flex flex-col rounded-md p-12 '>
+        <h1 className='pb-8 text-2xl'>Password Generator</h1>
+        <p className='border-2 border-white p-2 rounded-xl'>{password}</p>
         <button
           onClick={getRandomPassword}
-          className='border-2 border-white rounded-md'
+          className='border-2 border-white rounded-xl p-2 mt-8'
         >
           Generate Password
         </button>
         <button
-          className='border-2 border-white rounded-md'
+          className='border-2 border-white rounded-xl p-2 mt-8'
           onClick={savePassword}
         >
           Copy Password
