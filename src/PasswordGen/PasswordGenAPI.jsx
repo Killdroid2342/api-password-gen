9a489c0172e50c09e77ff86ee608173a7a8d405a
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-19 02:32:42 +0000

    deleted all logic

--- src/PasswordGen/PasswordGenAPI.jsx
+++ src/PasswordGen/PasswordGenAPI.jsx
@@ -2,18 +2,12 @@ import React, { useState } from 'react';
 
 const PasswordGen = () => {
   document.title = 'Password Generator 2.0';
-  const [password, setPassword] = useState([{}]);
+  const [password, setPassword] = useState();
 
-  let API =
-    'https://passwordinator.herokuapp.com/generate?num=true&caps=true&char=true&len=8';
+  // let API =
+  //   'https://passwordinator.herokuapp.com/generate?num=true&caps=true&char=true&len=8';
 
-  const getRandomPassword = () => {
-    fetch(API)
-      .then((res) => res.json())
-      .then((data) => {
-        setPassword(data);
-      });
-  };
+  const getRandomPassword = () => {};
   // console.log(getRandomPassword);
   const savePassword = () => {
     navigator.clipboard.writeText(password);
@@ -23,15 +17,10 @@ const PasswordGen = () => {
     <div className='flex flex-col items-center justify-center my-auto h-screen text-center '>
       <div className=' border-2 border-white flex flex-col rounded-md p-12 '>
         <h1 className='pb-8 text-2xl'>Password Generator 2.0</h1>
-        {typeof password === 'undefined' ? (
-          <p>Waiting For Password...</p>
-        ) : (
-          <p className='border-2 border-white p-2 rounded-xl'>
-            {password.data}
-          </p>
-        )}
+
+        <p className='border-2 border-white p-2 rounded-xl'>{password}</p>
         <button
-          onClick={() => getRandomPassword()}
+          onClick={getRandomPassword}
           className='border-2 border-white rounded-xl p-2 mt-8'
         >
           Generate Password
