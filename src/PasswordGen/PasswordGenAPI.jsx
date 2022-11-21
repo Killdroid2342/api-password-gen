8c55a6a58ddd959bd1762a4c26c76a5404aa5048
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-21 01:34:48 +0000

    finished passwordAPI

--- src/PasswordGen/PasswordGenAPI.jsx
+++ src/PasswordGen/PasswordGenAPI.jsx
@@ -7,16 +7,20 @@ const PasswordGen = () => {
     'Click Button Below To Generate Password'
   );
 
+  const [errorMessage, setErrorMessage] = useState('');
   const getRandomPassword = async () => {
-    await axios
+    axios
       .get(
         'https://passwordinator.herokuapp.com/generate?num=true&caps=true&char=true&len=16'
       )
       .then((res) => {
+        console.log(res);
+        setErrorMessage('');
         setPassword(res.data.data);
       })
       .catch((err) => {
         console.log(err);
+        setErrorMessage('Wait a few seconds, stop spamming retard');
       });
   };
   // console.log(getRandomPassword);
@@ -30,12 +34,18 @@ const PasswordGen = () => {
         <h1 className='pb-8 text-2xl'>Password Generator 2.0</h1>
 
         <p className='border-2 border-white p-2 rounded-xl'>{password}</p>
+        {errorMessage !== '' && (
+          <button className='border-2 border-white rounded-xl p-2 mt-8'>
+            {errorMessage}
+          </button>
+        )}
         <button
           onClick={getRandomPassword}
           className='border-2 border-white rounded-xl p-2 mt-8'
         >
           Generate Password
         </button>
+
         <button
           className='border-2 border-white rounded-xl p-2 mt-8'
           onClick={savePassword}
